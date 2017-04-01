5a9f9a838a15761200389b4a1a534f5775faf9d2
kim kiju <kimkiju@d93130.lds.korea-u.ac.jp>
2017-04-01 18:40:04 +0900

    changeorder

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     user = User.find(params[:id])
-    @stats = user.stats.all.order("created_at DESC").limit(7)
+    @stats = user.stats.all.order("date DESC").limit(7)
     @user = user
   end
 

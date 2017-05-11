47d6070ebd682b07f57bcbd69c817560a57f7db5
kim kiju <kimkiju.1216@gmail.com>
2017-05-11 23:24:56 +0900

    3things_update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,18 +5,10 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  before_action :auth
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:year,:group_id,:image])
     devise_parameter_sanitizer.permit(:account_update, keys: [:year,:group_id,:image])
   end
 
-      def auth
-      name = 'surajan'
-      passwd = '2017'
-      authenticate_or_request_with_http_basic('book')do |n,p|
-      n == name && p == passwd
-      end
-      end
 end

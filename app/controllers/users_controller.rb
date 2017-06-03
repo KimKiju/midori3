2f5eaef8e056dc6262da9067f7d13129f9d5d7de
kim kiju <kimkiju.1216@gmail.com>
2017-06-03 23:40:25 +0900

    update_0603

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,9 @@ class UsersController < ApplicationController
 
   def show
     user = User.find(params[:id])
-    @stats = user.stats.all.order("date DESC").limit(7)
+    @stat_all = user.stats.all
+    @stats = user.stats.order("date DESC").limit(7)
+    @stat_chart = user.stats.order("created_at DESC").limit(7)
     @user = user
   end
 
